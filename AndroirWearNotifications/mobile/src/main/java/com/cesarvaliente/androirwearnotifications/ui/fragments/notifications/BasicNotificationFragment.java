62a8c3c7b5b48c4d83d0bf5645c358e79d0a7deb
Cesar Valiente <cesar@6wunderkinder.com>
2014-10-28 07:11:45 +0100

    * Added rest of the basic notification features samples

--- AndroirWearNotifications/mobile/src/main/java/com/cesarvaliente/androirwearnotifications/ui/fragments/notifications/BasicNotificationFragment.java
+++ AndroirWearNotifications/mobile/src/main/java/com/cesarvaliente/androirwearnotifications/ui/fragments/notifications/BasicNotificationFragment.java
@@ -2,6 +2,9 @@ package com.cesarvaliente.androirwearnotifications.ui.fragments.notifications;
 
 import android.app.PendingIntent;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.NotificationCompat;
 import android.support.v4.app.NotificationManagerCompat;
@@ -9,6 +12,7 @@ import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.CheckBox;
 import android.widget.EditText;
 
 import butterknife.ButterKnife;
@@ -16,6 +20,7 @@ import butterknife.InjectView;
 import com.cesarvaliente.androirwearnotifications.R;
 import com.cesarvaliente.androirwearnotifications.ui.activities.MainActivity;
 import com.cesarvaliente.androirwearnotifications.ui.activities.OpenActivity;
+import com.cesarvaliente.androirwearnotifications.ui.utils.WearableUtils;
 
 /**
  * Created by cesar on 25/10/14.
@@ -24,11 +29,25 @@ public class BasicNotificationFragment extends BaseNotificationFragment {
 
     private final int BASIC_NOTIFICATION_ID = 1;
 
+    private final int MAX_NOTIFICATION_CONTENT_LENGTH = 15;
+
     @InjectView(R.id.basic_title)
     EditText mTitleEditText;
 
     @InjectView(R.id.basic_content)
     EditText mContentEditText;
+
+    @InjectView(R.id.basic_map)
+    CheckBox mAction1Checkbox;
+
+    @InjectView(R.id.basic_just_wearable)
+    CheckBox mJustWearableCheckbox;
+
+    @InjectView(R.id.basic_hide_icon)
+    CheckBox mHideIconCheckbox;
+
+    @InjectView(R.id.basic_change_background)
+    CheckBox mChangeBackgroundCheckbox;
 
     /**
      * Returns a new instance of this fragment for the given section
@@ -60,7 +79,7 @@ public class BasicNotificationFragment extends BaseNotificationFragment {
         String content = checkContent();
 
         Intent viewIntent = new Intent(getActivity(), OpenActivity.class);
-        viewIntent.putExtra(OpenActivity.OPEN_INTENT_EXTRA, title + " " + content);
+        viewIntent.putExtra(OpenActivity.OPEN_TEXT_EXTRA, title + " " + content);
         viewIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
         PendingIntent pendingIntent = PendingIntent.getActivity(getActivity(), 0, viewIntent,
                 PendingIntent.FLAG_UPDATE_CURRENT);
@@ -71,6 +90,47 @@ public class BasicNotificationFragment extends BaseNotificationFragment {
                 .setContentText(content)
                 .setContentIntent(pendingIntent);
 
+        if (content.length() > MAX_NOTIFICATION_CONTENT_LENGTH) {
+            NotificationCompat.BigTextStyle bigTextStyle = new NotificationCompat.BigTextStyle();
+            bigTextStyle.bigText(content);
+            notificationBuilder.setStyle(bigTextStyle);
+        }
+
+        //Always use NotificationCompat since some features of the WearableExtender on the NotificationManager
+        //don't work
+        NotificationCompat.WearableExtender wearableExtender = null;
+
+        if (mAction1Checkbox.isChecked()) {
+            Intent mapIntent = new Intent(Intent.ACTION_VIEW);
+            Uri geoUri = Uri.parse("geo:0,0?q=" + Uri.encode("46.7833856,23.6165124(Hello world!!)"));
+            mapIntent.setData(geoUri);
+            PendingIntent mapPendingIntent = PendingIntent.getActivity(getActivity(), 0, mapIntent,
+                    PendingIntent.FLAG_UPDATE_CURRENT);
+            NotificationCompat.Action action = new NotificationCompat.Action.Builder(
+                    android.R.drawable.ic_dialog_map, getString(R.string.map),
+                    mapPendingIntent).build();
+
+            if (mJustWearableCheckbox.isChecked()) {
+                wearableExtender = WearableUtils.getWearableExtender(wearableExtender).addAction(action);
+            } else {
+                notificationBuilder.addAction(action);
+            }
+        }
+
+        if (mHideIconCheckbox.isChecked()) {
+            wearableExtender = WearableUtils.getWearableExtender(wearableExtender).setHintHideIcon(true);
+        }
+
+        if (mChangeBackgroundCheckbox.isChecked()) {
+            Bitmap bitmap = BitmapFactory.decodeResource(getResources(), R.drawable.common_ic_googleplayservices);
+            wearableExtender = WearableUtils.getWearableExtender(wearableExtender).setBackground(bitmap);
+        }
+
+        if (wearableExtender != null) {
+            notificationBuilder.extend(wearableExtender);
+        }
+
+        //Always use NotficiationManagerCompat since some functions don't work in the NotificationManager api
         NotificationManagerCompat notificationManager = NotificationManagerCompat.from(getActivity());
 
         notificationManager.notify(BASIC_NOTIFICATION_ID, notificationBuilder.build());
