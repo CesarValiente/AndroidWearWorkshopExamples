991604f94d1f4853525c0bbe4162a09f23dc7484
Cesar Valiente <cesar@6wunderkinder.com>
2014-10-28 01:17:56 +0100

    * Added abstract class to force all notification fragments implement the same functionality

---
+++ AndroirWearNotifications/mobile/src/main/java/com/cesarvaliente/androirwearnotifications/ui/fragments/notifications/BaseNotificationFragment.java
@@ -0,0 +1,11 @@
+package com.cesarvaliente.androirwearnotifications.ui.fragments.notifications;
+
+import android.app.Fragment;
+
+/**
+ * Created by cesar on 28/10/14.
+ */
+public abstract class BaseNotificationFragment extends Fragment {
+
+    public abstract void showNotification();
+}
