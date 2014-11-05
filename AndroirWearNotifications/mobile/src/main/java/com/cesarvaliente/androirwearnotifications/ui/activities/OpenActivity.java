a8574a2761c03a817d9ec5bbe17941ade78f5be0
Cesar Valiente <cesar@6wunderkinder.com>
2014-11-05 16:55:04 +0100

    * The activity now checks if it has voice content (from a possible voice notification)

--- AndroirWearNotifications/mobile/src/main/java/com/cesarvaliente/androirwearnotifications/ui/activities/OpenActivity.java
+++ AndroirWearNotifications/mobile/src/main/java/com/cesarvaliente/androirwearnotifications/ui/activities/OpenActivity.java
@@ -3,11 +3,14 @@ package com.cesarvaliente.androirwearnotifications.ui.activities;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.app.RemoteInput;
+import android.text.TextUtils;
 import android.widget.TextView;
 
 import butterknife.ButterKnife;
 import butterknife.InjectView;
 import com.cesarvaliente.androirwearnotifications.R;
+import com.cesarvaliente.androirwearnotifications.ui.fragments.notifications.VoiceNotificationFragment;
 
 /**
  * Created by cesar on 27/10/14.
@@ -32,11 +35,17 @@ public class OpenActivity extends Activity {
     protected void onResume() {
         super.onResume();
 
-        Bundle bundle = getIntent().getExtras();
-        if (bundle != null && bundle.containsKey(OPEN_TEXT_EXTRA)) {
-            String extra = bundle.getString(OPEN_TEXT_EXTRA);
-            mTextView.setText(extra);
+        String voiceMessage = getVoiceMessage(getIntent());
+        if (!TextUtils.isEmpty(voiceMessage)) {
+            mTextView.setText(voiceMessage);
+        } else {
+            Bundle bundle = getIntent().getExtras();
+            if (bundle != null && bundle.containsKey(OPEN_TEXT_EXTRA)) {
+                String extra = bundle.getString(OPEN_TEXT_EXTRA);
+                mTextView.setText(extra);
+            }
         }
+
     }
 
     @Override
@@ -44,5 +53,20 @@ public class OpenActivity extends Activity {
         super.onNewIntent(intent);
 
         setIntent(intent);
+    }
+
+    /**
+     * Gets the voice reply that we have been used in the {@link VoiceNotificationFragment}
+     * (Voice notification example)
+     * @param intent
+     * @return
+     */
+    private String getVoiceMessage(Intent intent) {
+
+        Bundle remoteInput = RemoteInput.getResultsFromIntent(intent);
+        if (remoteInput != null && remoteInput.containsKey(VoiceNotificationFragment.EXTRA_VOICE_REPLY)) {
+            return remoteInput.getCharSequence(VoiceNotificationFragment.EXTRA_VOICE_REPLY).toString();
+        }
+        return null;
     }
 }
