8e14f457c29f2b4173cf5e6245904257296efd53
Cesar Valiente <cesar@6wunderkinder.com>
2014-11-05 18:39:38 +0100

    * All constants are now in this class

---
+++ AndroirWearNotifications/mobile/src/main/java/com/cesarvaliente/androirwearnotifications/ui/utils/Constants.java
@@ -0,0 +1,17 @@
+package com.cesarvaliente.androirwearnotifications.ui.utils;
+
+/**
+ * Created by cesar on 05/11/14.
+ */
+public class Constants {
+
+    //Notification types Ids
+    public static final int BASIC_NOTIFICATION_ID = 1;
+    public static final int VOICE_NOTIFICATION_ID = 2;
+    public static final int PAGES_NOTIFICATION_ID = 3;
+
+    //Extras
+    public static final String RESULT_TEXT_EXTRA = "result_text_extra";
+    public static final String EXTRA_VOICE_REPLY = "extra_voice_reply";
+
+}
